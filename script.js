20cdb8d5b14000d80f8cd2197bb90b289124a37a
Stelmachonak <362060@mail.ru>
2020-03-21 18:39:23 +0300

    feat: add solutions for portfolio border and mix picture

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 const MENU = document.getElementById("menu");
 
 MENU.addEventListener("click", event => {
-  MENU.querySelectorAll("a").forEach(el => el.classList.remove("active"));
+  MENU.querySelectorAll(".nav__link").forEach(el =>
+    el.classList.remove("active")
+  );
   event.target.classList.add("active");
 });
 
@@ -28,6 +30,46 @@ iphoneHorizontal.onclick = function() {
 
 /* portfolio border on imges*/
 
-let portfolioPicture = document.querySelectorAll(".img__item");
+let portfolioPictures = document.querySelectorAll(".img__item");
+let activeImg = null;
 
+for (let i = 0; i < portfolioPictures.length; i++) {
+  portfolioPictures[i].addEventListener("click", function() {
+    if (activeImg) {
+      activeImg.classList.remove("active");
+    }
+    activeImg = this;
+    this.classList.add("active");
+  });
+}
 
+/* switch picture position */
+
+const portfolioNav = document.getElementById("portfolionav");
+const pictureSlider = document.querySelector(".picture__slider");
+const all = document.querySelector(".all");
+const web = document.querySelector(".web_design");
+const graphic = document.querySelector(".graphic_design");
+const atwork = document.querySelector(".atwork");
+
+let arrPortfoliopictures = [].slice.call(portfolioPictures);
+
+portfolioNav.addEventListener("click", event => {
+  portfolioNav
+    .querySelectorAll(".portfolio__nav__list_item")
+    .forEach(el => el.classList.remove("active"));
+  event.target.classList.add("active");
+});
+
+all.addEventListener("click", randomMix);
+web.addEventListener("click", randomMix);
+graphic.addEventListener("click", randomMix);
+atwork.addEventListener("click", randomMix);
+
+function randomMix(arr) {
+  for (let i = arrPortfoliopictures.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrPortfoliopictures[i], arrPortfoliopictures[j]] = [arrPortfoliopictures[j], arrPortfoliopictures[i]]; 
+    pictureSlider.appendChild(arrPortfoliopictures[i]);
+}
+}
